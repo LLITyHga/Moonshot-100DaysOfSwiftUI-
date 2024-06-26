819ba50b4fb86b3c5bde8497e8000d9a0366447f
Андрій Пікусь <llityhga@gmail.com>
2024-06-26 14:47:08 +0300

    Initial Commit

---
+++ Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Wolf on 26.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
